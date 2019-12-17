cda961ebc402020ccf9d978aaa65a2a0bfdbe006
Consini <2459971164@qq.com>
2019-12-17 22:53:43 +0800

    字符串变换

---
+++ 算法训练/src/字符串变换.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * @Description TODO
+ * @Author K
+ * @Date 2019/12/16 16:02
+ **/
+/*1 表示全部转化为大写字母输出，如abC 变成 ABC
+　　2 表示全部转换为小写字母输出，如abC变成abc
+　　3 表示将字符串整个逆序输出，如 abc 变成 cba
+　　4 表示将字符串中对应的大写字母转换为小写字母，而将其中的小写字母转化为大写字母输出，如 abC变成ABc
+　　5表示将全部转换为小写字母，并将其中所有的连续子串转换为对应的缩写形式输出，
+比如abcD 转换为a-d，其次，-至少代表1个字母，既如果是ab，则不需要转换为缩写形式。*/
+public class 字符串变换 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
