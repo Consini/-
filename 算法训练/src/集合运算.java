2b58f8d095f3ad8e3d4c9eb44b08c27c854139f0
Consini <2459971164@qq.com>
2020-01-07 22:52:55 +0800

    集合运算

---
+++ 算法训练/src/集合运算.java
@@ -0,0 +1,27 @@
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.util.*;
+
+/**
+ * @Description TODO
+ * @Author K
+ * @Date 2019/12/16 16:19
+ **/
+/*问题描述
+　　给出两个整数集合A、B，求出他们的交集、并集以及B在A中的余集。
+输入格式
+　　第一行为一个整数n，表示集合A中的元素个数。
+　　第二行有n个互不相同的用空格隔开的整数，表示集合A中的元素。
+　　第三行为一个整数m，表示集合B中的元素个数。
+　　第四行有m个互不相同的用空格隔开的整数，表示集合B中的元素。
+　　集合中的所有元素均为int范围内的整数，n、m<=1000。
+输出格式
+　　第一行按从小到大的顺序输出A、B交集中的所有元素。
+　　第二行按从小到大的顺序输出A、B并集中的所有元素。
+　　第三行按从小到大的顺序输出B在A中的余集中的所有元素。*/
+public class 集合运算 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
